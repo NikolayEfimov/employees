6936d1d8f0a1a557f6b69fcc4d5c7340c4f8b320
Nikolai Efimov <Nikolai.Efimov@german-edge-cloud.io>
2024-05-14 22:27:25 +0300

    create project template

---
+++ src/main/java/sandbox/challenge/employees/EmployeesApplication.java
@@ -0,0 +1,13 @@
+package sandbox.challenge.employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sandbox/challenge/employees/EmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package sandbox.challenge.employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
